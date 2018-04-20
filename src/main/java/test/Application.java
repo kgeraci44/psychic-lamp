7861b9e094d456eed0853c8788ea0f2aa2c7c772
Kevin <Kevin@KGDos.fios-router.home>
2018-04-20 08:13:39 -0400

    Added local mySQL database support

--- src/main/java/test/Application.java
+++ src/main/java/test/Application.java
@@ -3,7 +3,10 @@ package test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+//Cant get multiple packages to work!
+//@ComponentScan(basePackageClasses= { GreetingController.class, MusicDetails.class, MusicDetailsRepository.class})
 public class Application {
 
     public static void main(String[] args) {
